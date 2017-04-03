a589bdb9112cba622921e4d70bac7a8b2706373a
Aryan Gupta <kuantum.Freak@gmail.com>
2017-04-03 18:30:48 -0400

    Added Player Class

---
+++ inc/Player.h
@@ -0,0 +1,25 @@
+/* 
+ * Copyright (c) 2017 The Gupta Empire - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by Aryan Gupta <me@theguptaempire.net>
+ * 
+ * =============================================================================
+ * @author 			Aryan Gupta
+ * @project 		
+ * @title 			
+ * @date			(YYYY-MM-DD)
+ * @fversion		1.0.0
+ * @description 	
+ * =============================================================================
+ */
+
+#ifndef PLAYER_H_INC
+#define PLAYER_H_INC
+
+class Player {
+
+};
+
+#endif
