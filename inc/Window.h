01d15b7bd62c7d80ecf86ef8910afbf21c8d8926
Aryan Gupta <kuantum.Freak@gmail.com>
2017-04-05 17:41:54 -0400

    Set up Files

---
+++ inc/Window.h
@@ -0,0 +1,29 @@
+/* 
+ * Copyright (c) 2017 The Gupta Empire - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by Aryan Gupta <me@theguptaempire.net>
+ * 
+ * =============================================================================
+ * @author 			Aryan Gupta
+ * @project 		
+ * @title 			
+ * @date			(YYYY-MM-DD)
+ * @fversion		1.0.0
+ * @description 	
+ * =============================================================================
+ */
+
+#ifndef WINDOW_H_INC
+#define WINDOW_H_INC
+
+
+class Window {
+public:
+
+private:
+};
+
+
+#endif
