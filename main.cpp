66d8f75b6193a6cf26cec39bc1e9c350f82c20ec
Aryan Gupta <kuantum.Freak@gmail.com>
2017-04-09 21:15:45 -0400

    Finish Opponent Class

--- main.cpp
+++ main.cpp
@@ -29,6 +29,7 @@ using std::vector;
 #include ".\inc\CardPile.h"
 #include ".\inc\Player.h"
 #include ".\inc\Human.h"
+#include ".\inc\Opponent.h"
 #include ".\inc\Window.h"
 #include ".\inc\Resources.h"
 
@@ -55,7 +56,7 @@ int main(int argc, char* argv[]) {
 	gDeck    = new CardPile(PILE_DECK);
 	gDiscard = new CardPile(PILE_DISCARD);
 	P1       = new Human();
-	P2       = new Human();
+	P2       = new Opponent();
 	
 	gDeck->shuffle(); // Shuffle our main deck
 	
