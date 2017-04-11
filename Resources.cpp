dc59322b9753191f3b06d33c184af655415ce608
Aryan Gupta <kuantum.Freak@gmail.com>
2017-04-11 07:45:48 -0400

    Add Proper Grammer for Help Text

--- Resources.cpp
+++ Resources.cpp
@@ -39,11 +39,11 @@ static const char* UI_SHEETS[] = {
 };
 
 const char* HELP_TEXT[] = {
-	"Its your turn. Pick a deck to pick from. You can either click on the stock deck or the discard deck",
-	"You MUST draw from either card pile. Please click on either the stock deck or the discard deck",
+	"Its your turn. Pick a deck to pick from. You can either click on the stock deck or the discard deck.",
+	"You MUST draw from either card pile. Please click on either the stock deck or the discard deck.",
 	"Now you must discard a card, or knock if you can",
-	"You MUST click on a card, or knock if you can. You can knock ONLY if you have less than 10 deadwood",
-	"Its your Opponent's turn"	
+	"You MUST click on a card, or knock if you can. You can knock ONLY if you have less than 10 deadwood.",
+	"Its your Opponent's turn."	
 };
 
 Resources::Resources() {
