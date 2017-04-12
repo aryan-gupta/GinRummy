fbf6ff34fdd2e7d0644fff4b86273b2e68dcec41
Unknown <pkulapru@uncc.edu>
2017-04-11 21:13:24 -0400

    BigGin function

--- Player.cpp
+++ Player.cpp
@@ -159,8 +159,8 @@ unsigned Player::getGin() {
 
 unsigned Player::getBigGin() {
 	// When all 10 cards in our hand are parts of melds and the card we just drew are parts of melds - 11 cards total
+	if(deadwood.size == 0) 
 	
 	
-	
-	return 0;
+	return 30;
 }
