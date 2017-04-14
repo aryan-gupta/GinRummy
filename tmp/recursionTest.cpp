eeb548437c6e4a1f04c22c85e57b511220aa16a7
Aryan Gupta <kuantum.Freak@gmail.com>
2017-04-13 22:10:15 -0400

    Recursion Test Sucessful

--- tmp/recursionTest.cpp
+++ tmp/recursionTest.cpp
@@ -8,30 +8,29 @@ typedef vector<int> IV;
 
 IV master;
 
-void func(int start, int depth, int fDepth, IV vec, vector<IV> &pos) {
-	cout << depth << start << endl;
+void func(int start, int depth, const int &fDepth, IV vec, vector<IV> &pos) {
+	vec.push_back(master[start]);
+	
 	if(depth == fDepth) {
 		pos.push_back(vec);
 		return;
 	}
 	
-	vec.push_back(master[start]);
-	for(; start < master.size(); ++start) {
-		func(start + 1, depth + 1, fDepth, vec, pos);
+	for(int i = start + 1; i < master.size(); ++i) {
+		func(i, depth + 1, fDepth, vec, pos);
 	}
-
 }
 
 int main() {
 	master = {1, 2, 3, 4};
 	
-	vector<IV> pos(master.size());
+	vector<IV> pos;
 	
-	//for(int  i = 0; i < master.size(); ++i) {
-		IV one;
-		func(0, 0, 2, one, pos);
+	IV vec;
+	for(int maxDepth = 0; maxDepth < master.size(); ++maxDepth)
+		for(int start = 0; start < master.size(); ++start)
+			func(start, 0, maxDepth, vec, pos);
 		
-	//}
 	
 	for(IV& tmpIV : pos) {
 		for(int tmpInt : tmpIV) {
