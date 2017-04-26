afa1ec07d5dcaf9c4b7b48a37f7282207a0d9395
Aryan Gupta <kuantum.freak@gmail.com>
2017-04-26 18:55:32 -0400

    Update Info for Release

--- info.h
+++ info.h
@@ -22,18 +22,18 @@
 #define INFO_H_INCLUDED
 
 // =================  PROGRAM  INFO  =================
-#define R_PVERSION	       /* Version Number */ "1.0.0"
+#define R_PVERSION	       /* Version Number */ "1.1.0"
  
-#define R_PROJNAME		     /* Project Name */ "Gin Rummy"
-#define R_PRGMNAME		     /* Program Name */ "Gin Rummy"
-#define R_DESCRIPTION	      /* Description */ "A game of Gin Rummy"
+#define R_PROJNAME		     /* Project Name */ "GinRummy"
+#define R_DESCRIPTION	      /* Description */ "Gin Rummy"
 
 #define R_AUTHOR		     	   /* Author */ "Aryan Gupta, Pichsapa Kulapruk, and Harison Read"
 #define R_COMPANY		          /* Company */ "The Cool Group"
 #define R_COPYRIGHT				/* Copyright */ "Copyright (c) The Cool Group - All Rights Reserved"
-#define R_ORGFILENAME  /* Original File Name */ "main.exe" 
+#define R_ORGFILENAME  /* Original File Name */ "main.exe"
+#define R_PRGMNAME		     /* Program Name */ "main.exe" 
 #define R_FVERSION		     /* File Version */ "1.0.0"
-#define R_COMMENTS		         /* Comments */  ""
+#define R_COMMENTS		         /* Comments */ "A Game of Gin Rumme"
 #define R_LEGALTRDMKS	  /* Legal Tademarks */ "..."
 #define R_PRIVATEBUILD	    /* Private Build */ "\0" 
 #define R_SPECIALBUILD	    /* Special Build */ "\0"
